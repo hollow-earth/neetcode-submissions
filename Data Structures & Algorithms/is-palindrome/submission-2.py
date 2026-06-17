bde08aea6014a29e4a531870627e245bf0909d6f
hollow-earth <71574681+hollow-earth@users.noreply.github.com>
2026-06-16 20:38:30 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        c = ""
+        for char in s:
+            if char.isalnum():
+                c += char.lower()
+        return c == c[::-1]
