b436b8c021a47268eb5398c051d9d01bcf13826c
hollow-earth <71574681+hollow-earth@users.noreply.github.com>
2026-06-16 22:54:37 -0400

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.py
@@ -0,0 +1,10 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        res = defaultdict(int)
+        res_list = []
+        for i in nums:
+            res[i] += 1
+        res = sorted([(value,key) for key,value in res.items()], reverse=True)
+        for i in range(0,k):
+            res_list.append(res[i][1])
+        return res_list
