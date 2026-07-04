2f643f054e9d312b7b394841b606d2c5a6394e0c
hollow-earth <71574681+hollow-earth@users.noreply.github.com>
2026-07-03 20:23:21 -0400

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        res = ""
+
+        for i in range(min(len(word1), len(word2))):
+            res += word1[i]
+            res += word2[i]
+        if len(word1) > len(word2):
+            res += word1[len(word2):]
+        elif len(word1) < len(word2):
+            res += word2[len(word1):]
+        return res
