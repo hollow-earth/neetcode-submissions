cc1864792676870cebe5c62e495607bf01d34412
hollow-earth <71574681+hollow-earth@users.noreply.github.com>
2026-07-28 13:02:25 -0400

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_freq = [0] * 26
+        s2_freq = [0] * 26
+        if len(s1) > len(s2):
+            return False
+        
+        l_ptr, r_ptr = 0, len(s1) - 1
+        for c in s1:
+            s1_freq[ord(c)-97] += 1
+        for c in s2[l_ptr:r_ptr+1]:
+            s2_freq[ord(c)-97] += 1
+        
+        while r_ptr < len(s2) - 1:
+            if s1_freq == s2_freq:
+                return True
+            else:
+                s2_freq[ord(s2[l_ptr])-97] -= 1
+                l_ptr += 1
+                r_ptr += 1
+                s2_freq[ord(s2[r_ptr])-97] += 1
+        if s1_freq == s2_freq:
+                return True
+    
+        return False
